e1dc1c3e64da84b9ff5efef4fc8fadae7713f018
Jide <jideabdqudus@gmail.com>
2025-04-13 01:52:09 +0100

    swagger docs implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Tasks API')
+    .setDescription('Task Management API')
+    .addBearerAuth({
+      type: 'http',
+      name: 'JWT',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
